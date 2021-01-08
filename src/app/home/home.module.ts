f356f34978efd750d8c766ff8f1497f060b69375
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-08 14:02:49 +0100

    Lazy load adn PreloadAllModules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   declarations: [BannerComponent, HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
