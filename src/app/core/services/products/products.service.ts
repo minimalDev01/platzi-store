abbca0b7517e51bad176e862707778c13b7f8647
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-09 21:08:17 +0100

    Http client changes

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Product } from '../../models/product.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +11,10 @@ export class ProductsService {
   constructor(private http: HttpClient) {}
 
   getAllProducts() {
-    return this.http.get<Product[]>(
-      'https://platzi-store.herokuapp.com/products/'
-    );
+    return this.http.get<Product[]>(`${environment.url_api}/products/`);
   }
 
   getProduct(id: string) {
-    return this.http.get(`https://platzi-store.herokuapp.com/products/${id}`);
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
   }
 }
