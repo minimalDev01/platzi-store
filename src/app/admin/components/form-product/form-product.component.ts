625ba0ee6ad7f81be0feb635c1d4edece619150e
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-11 14:31:40 +0100

    FormGroup, FormBuilder added

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from '../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
-  styleUrls: ['./form-product.component.scss']
+  styleUrls: ['./form-product.component.scss'],
 })
 export class FormProductComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((newProduct) => {
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: [0, [Validators.required]],
+      image: '',
+      description: ['', [Validators.required]],
+    });
+  }
 }
