9460854ef6aad498f5150753760e41d25d9e84fe
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-11 14:01:05 +0100

    CSS minor changes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { TableComponent } from './components/table/table.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { FormProductComponent } from './components/form-product/form-product.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ProductsListComponent } from './components/products-list/products-list.
     TableComponent,
     DashboardComponent,
     ProductsListComponent,
+    FormProductComponent,
   ],
   imports: [
     CommonModule,
