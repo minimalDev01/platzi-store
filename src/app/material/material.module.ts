b031daf64cee9cb533d68953686f5f8284d92960
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-11 13:37:10 +0100

    Email contact reactive form added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -17,6 +17,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
@@ -38,6 +39,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSortModule,
     MatGridListModule,
     MatMenuModule,
+    MatFormFieldModule,
   ],
   exports: [
     LayoutModule,
@@ -56,6 +58,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSortModule,
     MatGridListModule,
     MatMenuModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
