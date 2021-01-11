1173d636ffb9d5758a1197b1509d5c0dfa19aeee
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-11 21:17:37 +0100

    Reactive cart

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -1,50 +1,25 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  OnChanges,
-  SimpleChanges,
-  OnInit,
-  DoCheck,
-  OnDestroy,
-} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
 import { Product } from '../../../core/models/product.model';
+
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit, DoCheck, OnDestroy {
+export class ProductComponent implements OnInit {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
   today = new Date();
 
-  constructor() {
-    //console.log('1. constructor');
-  }
+  constructor(private cartService: CartService) {}
 
-  //   ngOnChanges(changes: SimpleChanges) {
-  //     console.log('2. ngOnChanges');
-  //     console.log(changes);
-  //   }
-
-  ngOnInit() {
-    // console.log('3. ngOnInit');
-  }
-
-  ngDoCheck() {
-    // console.log('4. ngDoCheck');
-  }
-
-  ngOnDestroy() {
-    // console.log('5. ngOnDestroy');
-  }
+  ngOnInit() {}
 
   addCart() {
-    // console.log('añadir al carrito');
-    this.productClicked.emit(this.product.id);
+    this.cartService.addCart(this.product);
   }
 }
