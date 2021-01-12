522bfbf52a0ece77cc4a13a603734604b35b24aa
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-12 13:24:23 +0100

    Catching errs

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Product } from '@core/models/product.model';
 import { environment } from '@environments/environment';
@@ -11,22 +13,37 @@ export class ProductsService {
   constructor(private http: HttpClient) {}
 
   getAllProducts() {
-    return this.http.get<Product[]>(`${environment.url_api}/products/`);
+    return this.http
+      .get<Product[]>(`${environment.url_api}/products/`)
+      .pipe(catchError(this.handleError));
   }
 
   getProduct(id: string) {
-    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
+    return this.http
+      .get<Product>(`${environment.url_api}/products/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   createProduct(product: Product) {
-    return this.http.post(`${environment.url_api}/products`, product);
+    return this.http
+      .post(`${environment.url_api}/products`, product)
+      .pipe(catchError(this.handleError));
   }
 
   updateProduct(id: string, changes: Partial<Product>) {
-    return this.http.put(`${environment.url_api}/products/${id}`, changes);
+    return this.http
+      .put(`${environment.url_api}/products/${id}`, changes)
+      .pipe(catchError(this.handleError));
   }
 
   deleteProduct(id: string) {
-    return this.http.delete(`${environment.url_api}/products/${id}`);
+    return this.http
+      .delete(`${environment.url_api}/products/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+    return throwError('Something went wrong...');
   }
 }
