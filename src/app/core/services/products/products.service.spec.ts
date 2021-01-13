5aa96974a08d44931cbc9a3c09317d230f1ca7f1
Sergio Carralero <sergiocn.dev@gmail.com>
2021-01-13 14:33:43 +0100

    Unit tests with karma and jasmine, using coverage parameter

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,16 +1,70 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { ProductsService } from './products.service';
+import { ProductsService } from '@core/services/products/products.service';
+import { environment } from '@environments/environment';
 
-describe('ProductsService', () => {
+fdescribe('ProductsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for getAllProducts', () => {
+    it('should return products', () => {
+      // Arrange
+      const expectData = [
+        {
+          id: '1',
+          title: 'Testing 1',
+          price: 10,
+          description: 'Testing products',
+          image: 'img/img.png',
+        },
+        {
+          id: '2',
+          title: 'Testing 2',
+          price: 10,
+          description: 'Testing products',
+          image: 'img/img.png',
+        },
+      ];
+      // Act
+      let dataError, dataResponse;
+      service.getAllProducts().subscribe(
+        (response) => {
+          dataResponse = response;
+        },
+        (error) => {
+          dataError = error;
+        }
+      );
+      const req = httpTestingController.expectOne(
+        `${environment.url_api}/products/`
+      );
+      req.flush(expectData);
+
+      // Asort
+      expect(dataResponse.length).toEqual(2);
+      expect(req.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+    });
   });
 });
